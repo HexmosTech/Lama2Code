2bd01a31113f75289c0de28591e01d0a36ee8590
Shrijith Venkatramana <shrijith@hexmos.com>
2022-11-21 21:29:10 +0530

    Remove unnecessary alerts

--- media/script.js
+++ media/script.js
@@ -7,16 +7,20 @@ $(document).ready(function () {
         let j = JSON.parse(respBody)
         var j2ht = new J2HConverter(respBody, "responsebody");
         j2ht.convert();
+        /*
         vscode.postMessage({
             command: 'alert',
             text: 'j is json ;' + respBody.slice(0, 100)
         })
+        */
         var tree = jsonTree.create(j, $('#responsebody'));
     } catch (e) {
         console.log("j is not json")
+        /*
         vscode.postMessage({
             command: 'alert',
             text: 'j is not json ' + respBody.slice(0, 100)
         })
+        */
     }
 });
