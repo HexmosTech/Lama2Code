0535d450b6ec95e1dcd5020bb6f28841d7ac2d0f
lovestaco <athreyac4@gmail.com>
2023-03-04 15:24:29 +0530

    Auto suggesting variables when declaring one in any.l2 file.
    - Making use of `activeTextEditor` for getting the l2.env file.
    - Making use of `registerCompletionItemProvider` for suggesting the enc variables in the any.l2
    - This suggestion activates when user tries to import env variable using syntax `${` and triggers suggestion when `{` is used.

--- src/extension.ts
+++ src/extension.ts
@@ -1,100 +1,59 @@
 import * as vscode from "vscode";
-import ExecuteCurrentFile from "./executeCurrentFile";
-import LanguagesData from "./languages";
-
-interface LanguageData {
-  info: {
-    key: string;
-    title: string;
-    extname: string;
-    default: string;
-  };
-  clientsById: Record<string, null>;
-}
-
-interface LanguagesData {
-  [key: string]: LanguageData;
-}
-
+import * as fs from "fs";
+import * as path from "path";
 export function activate(context: vscode.ExtensionContext) {
   console.log('>>> Congratulations, your extension "Lama2" is now active!');
-
-  // Level1 command pallette
-  let executeCurrentFile = new ExecuteCurrentFile(context);
-
-  let executeCurrentFileDisposable = vscode.commands.registerCommand(
-    "lama2.ExecuteCurrentFile",
-    () => executeCurrentFile.execFile()
-  );
-
-  context.subscriptions.push(executeCurrentFileDisposable);
-
-  // Level1 command pallette
-  let generateCodeSnippetDisposable = vscode.commands.registerCommand(
-    "lama2.GenerateCodeSnippet",
-    async () => {
-      const languageKeys = Object.keys(LanguagesData);
-
-      // Level2 command pallette
-      const language: string | undefined = await vscode.window.showQuickPick(
-        languageKeys.map(
-          (key) => LanguagesData[key as keyof typeof LanguagesData].info.title
-        )
-      );
-
-      if (language) {
-        const languageKey = languageKeys.find(
-          (key) =>
-            LanguagesData[key as keyof typeof LanguagesData].info.title ===
-            language
-        )!;
-        const clientsById =
-          LanguagesData[languageKey as keyof typeof LanguagesData].clientsById;
-        console.log(clientsById);
-        const defaultClient =
-          LanguagesData[languageKey as keyof typeof LanguagesData].info.default;
-
-        const clientKeys = Object.keys(clientsById);
-
-        if (clientKeys.length === 1) {
-          const client = clientKeys[0];
-          console.log("Convert ", language, client);
-        } else {
-          // Level3 command pallette
-          const selection:
-            | {
-                label: string;
-                language: string;
-                client: string;
-              }
-            | undefined = await vscode.window.showQuickPick(
-            clientKeys.map((client) => ({
-              label: `${language}: ${client} ${isDefault(
-                defaultClient,
-                client
-              )}`,
-              language: language,
-              client: client,
-            }))
-          );
-
-          if (selection) {
-            console.log("Convert ", selection.language, selection.client);
-          }
-        }
+  let envVariables = [] as string[];
+  const activeTextEditor = vscode.window.activeTextEditor;
+  if (activeTextEditor) {
+    const activeFilePath = activeTextEditor.document.uri.fsPath;
+    const envFilePath = path.join(path.dirname(activeFilePath), "l2.env");
+    fs.readFile(envFilePath, "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
       }
-    }
+
+      envVariables = data
+        .split("\n")
+        .filter((line) => line.startsWith("export"))
+        .map((line) => line.split("=")[0].replace("export ", ""));
+      console.log("envVariables -> ", envVariables);
+    });
+  }
+
+  let suggestEnvVariables = vscode.languages.registerCompletionItemProvider(
+    { language: "lama2", scheme: "file" },
+    {
+      // eslint-disable-next-line no-unused-vars
+      provideCompletionItems(document, position, token, context) {
+        // get all text until the `position` and check if it reads `${`
+
+        const linePrefix = document
+          .lineAt(position)
+          .text.substring(0, position.character);
+        if (!linePrefix.endsWith("${")) {
+          return undefined;
+        }
+        let createSuggestion = (text: string) => {
+          let item = new vscode.CompletionItem(
+            text,
+            vscode.CompletionItemKind.Text
+          );
+          item.range = new vscode.Range(position, position);
+          return item;
+        };
+        const suggestionsArray = envVariables.map((item, index) => {
+          return createSuggestion(item);
+        });
+
+        return suggestionsArray;
+      },
+    },
+    "{" // trigger
   );
-  context.subscriptions.push(generateCodeSnippetDisposable);
+  context.subscriptions.push(suggestEnvVariables);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
-
-function isDefault(defaultClient: string, client: string) {
-  if (defaultClient == client) {
-    return "(Default)";
-  } else {
-    return "";
-  }
-}
