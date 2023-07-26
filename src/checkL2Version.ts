c83bb16d83d0c123257f26f23def26becb841499
lovestaco <athreyac4@gmail.com>
2023-07-26 10:22:21 +0530

    Using semver library to check versions

--- src/checkL2Version.ts
+++ src/checkL2Version.ts
@@ -3,46 +3,33 @@ import * as semver from "semver";
 import * as vscode from "vscode";
 import { getShowLama2Term } from "./utils";
 
-const MIN_VERSION_TO_CHECK = "1.5.1";
 const LAMA2_TERM_NAME = "AutoLama2";
 const UPDATE_MSG = "Support for environment variables.";
 
-export function checkL2Version() {
-  try {
-    getL2Version((l2Version) => {
-      // Check if l2Version is less than MIN_VERSION_TO_CHECK
-      if (semver.lt(l2Version, MIN_VERSION_TO_CHECK)) {
-        showUpdateWarning();
-      }
-    });
-  } catch (e: any) {
-    console.log("Problem while checking for version -> ", e);
-  }
-}
-
-function getL2Version(callback: (version: string) => void) {
+export async function getL2VersionAndUpdatePrompt(minVersionToCheck: string) {
   try {
     const l2Version = execSync("l2 --version", { encoding: 'utf-8' }).trim();
     // Use the semver library to validate and normalize the version string
     const normalizedVersion = semver.valid(l2Version);
     if (normalizedVersion) {
-      callback(normalizedVersion);
+
+      if (semver.lt(l2Version, minVersionToCheck)) {
+        showUpdateWarning(minVersionToCheck);
+      }
+      return normalizedVersion;
     } else {
       throw new Error("Invalid version format: " + l2Version);
     }
-  }
-  catch (err: any) {
-    console.log("Error: getL2Version() ", err);
-    throw new Error("Problem while reading version");
-
+  } catch (e: any) {
+    console.log("Problem while checking for version -> ", e);
   }
 }
 
-function showUpdateWarning() {
+function showUpdateWarning(minVersionToCheck: string) {
   const updateAction: vscode.MessageItem = { title: "Update" };
   vscode.window
     .showWarningMessage(
-      `Your Lama2 version is outdated. \nPlease update to version ${MIN_VERSION_TO_CHECK} or above for the best experience.\nUpdate: ${UPDATE_MSG}`,
+      `Your Lama2 version is outdated. \nPlease update to version ${minVersionToCheck} or above for the best experience.\nUpdate: ${UPDATE_MSG}`,
       updateAction
     )
     .then((selectedAction) => {
