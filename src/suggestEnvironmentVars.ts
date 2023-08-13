a112a479a0463354542e0d6a69ef2c3c59d9a80a
lovestaco <athreyac4@gmail.com>
2023-08-13 14:56:18 +0530

    Prettify code

--- src/suggestEnvironmentVars.ts
+++ src/suggestEnvironmentVars.ts
@@ -8,6 +8,7 @@ let envVars = [] as string[];
 let cursorPosition = 0;
 
 export function getEnvsFromEnvCommand(typedEnvArg: string): {} {
+  console.log("typedEnvArg -> ", typedEnvArg);
   const editor = vscode.window.activeTextEditor;
   if (!editor) {
     return {};
@@ -21,7 +22,10 @@ export function getEnvsFromEnvCommand(typedEnvArg: string): {} {
   try {
     // Execute the command and read the stdout for JSON of all the env's
 
-    const commandOutput = execSync(`./build/l2 --env=${typedEnvArg} ${l2FilePath}`, { cwd: "/home/lovestaco/repos/Lama2", }).toString(); // For local debugging
+    const commandOutput = execSync(
+      `./build/l2 --env=${typedEnvArg} ${l2FilePath}`,
+      { cwd: "/home/lovestaco/repos/Lama2" }
+    ).toString(); // For local debugging
     // const commandOutput = execSync(`l2 --env=${typedEnvArg} ${l2FilePath}`).toString();
     const envMap = JSON.parse(commandOutput);
     return envMap;
@@ -104,7 +108,8 @@ export function replaceTextAfterEnvSelected(env: string) {
   if (editor) {
     let position = editor.selection.active;
     let lineText = editor.document.lineAt(position.line).text;
-    let linePosition = editor.document.lineAt(position.line).range.start.character;
+    let linePosition = editor.document.lineAt(position.line).range.start
+      .character;
 
     let openingBraceIndex = lineText.lastIndexOf("${", linePosition);
     let closingBraceIndex = lineText.indexOf("}", openingBraceIndex);
