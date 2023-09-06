4541f89de97750a900095a879e05ecdb8ab42c3b
lovestaco <athreyac4@gmail.com>
2023-09-06 22:57:26 +0530

    Adding more detailed comments for the functions

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as semver from "semver";
 
 import { genCodeSnip } from './generateCodeSnippet';
 import { getRemoteUrl } from './getRemoteUrl';
-import { getDotENVS, lama2RegisterCompletionItemProvider, replaceTextAfterEnvSelected, suggestENVSFromDotEnv } from './suggestEnvironmentVars';
+import { getDotENVS, lama2RegisterCompletionItemProvider, replaceTextAfterEnvSelected, suggestENVSFromDotEnv, triggerSuggestionInTemplateLiteral } from './suggestEnvironmentVars';
 import { genLama2Examples } from './genLama2Examples';
 import { execCurL2File } from './executeCurrentFile';
 import { prettifyL2File } from './prettifyL2File';
@@ -49,6 +49,7 @@ export function activate(context: vscode.ExtensionContext) {
   let generateCodeSnippetDisposable = genCodeSnip();
   context.subscriptions.push(generateCodeSnippetDisposable);
   console.log(">>> generateCodeSnippetDisposable is now active!");
+
   // Automatically check L2 version on extension activation
   let suggestEnvVariables: any;
   let l2Version = getL2VersionAndUpdatePrompt(MIN_VERSION_TO_CHECK);
@@ -57,25 +58,41 @@ export function activate(context: vscode.ExtensionContext) {
     l2Version === undefined ||
     semver.lt(l2Version, MIN_VERSION_TO_CHECK)
   ) {
-    // The L2 version, if less than v1.5.0, does not support the `l2 -e <filepath>` feature.
-    // Therefore, we use the extension to fetch variables from the l2.env file for suggestions.
+    /**
+    * The L2 version, if less than v1.5.0, does not support the `l2 -e <filepath>` feature.
+    * Therefore, we use the extension to fetch variables from the l2.env file for suggestions.
+    */
     getDotENVS();
     suggestEnvVariables = suggestENVSFromDotEnv();
   } else {
-    // L2 versions greater than or equal to v1.5.0 support the `l2 -e <filepath>` feature.
-    // Therefore, we fetch variables from both the l2.env and l2config.env files for suggestions using this command.
+    /**
+     * L2 versions greater than or equal to v1.5.0 support the `l2 -e <filepath>` feature.
+     * Therefore, we fetch variables from both the l2.env and l2config.env files for suggestions using this command.
+     */
     suggestEnvVariables = lama2RegisterCompletionItemProvider(langServer, requestId);
   }
 
   context.subscriptions.push(
     suggestEnvVariables,
-    // The following part is unnecessary for L2 versions > 1.5.0
+    /**
+     * The following part is unnecessary for L2 versions > 1.5.0
+     * This method is triggered when a user selects a suggested environment variable.
+     */
     vscode.commands.registerCommand("envoptions", (selectedEnv: string) => {
-      // This method is triggered when a user selects a suggested environment variable.
       replaceTextAfterEnvSelected(selectedEnv);
     })
   );
 
+  /**
+   * Listens for text changes in the active document and triggers suggestion
+   * if the cursor is within a template literal inside string quotes.
+   * 
+   * This is needed because VS Code's native intellisense might not 
+   * automatically trigger suggestions within specific contexts like a 
+   * template literal inside string quotes.
+   */
+  const listener = triggerSuggestionInTemplateLiteral();
+  context.subscriptions.push(listener);
 }
 
 function initlizeServer() {
