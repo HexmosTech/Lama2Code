717b42e89f407e7bb644506a2d4dc2ca6d8cbc3d
lovestaco <athreyac4@gmail.com>
2023-09-06 23:07:04 +0530

    Lint code and refactor methods

---
+++ src/lsp/methods/lspLifecycles.ts
@@ -0,0 +1,48 @@
+import * as vscode from "vscode";
+import { ChildProcess } from "child_process";
+import { ILSPRequestDetails, sendRequestToLSPReadResponse } from "../utils";
+
+export function initlizeServer(langServer: ChildProcess, requestId: number) {
+  let initLspReq: ILSPRequestDetails = {
+    process: langServer,
+    id: requestId,
+    method: "initialize",
+    params: {
+      processId: process.pid,
+      rootUri: vscode.workspace.workspaceFolders
+        ? vscode.workspace.workspaceFolders[0].uri.toString()
+        : undefined,
+      workspace: {
+        workspaceFolders: {
+          supported: false,
+          changeNotifications: false,
+        },
+      },
+      clientInfo: {
+        name: vscode.env.appName,
+        version: vscode.version,
+      },
+    },
+  };
+  sendRequestToLSPReadResponse(initLspReq);
+}
+
+export function exitLsp(langServer: ChildProcess, requestId: number) {
+  requestId += 1;
+  let exitReq: ILSPRequestDetails = {
+    process: langServer,
+    id: requestId,
+    method: "exit",
+  };
+  sendRequestToLSPReadResponse(exitReq);
+}
+
+export function shutDownLsp(langServer: ChildProcess, requestId: number) {
+  requestId += 1;
+  let shutdownReq: ILSPRequestDetails = {
+    process: langServer,
+    id: requestId,
+    method: "shutdown",
+  };
+  sendRequestToLSPReadResponse(shutdownReq);
+}
