cd8f3772fa1eae132777930382381bd0e74e9a05
lovestaco <athreyac4@gmail.com>
2023-09-10 20:34:34 +0530

    Cleaning up v1.5.1 deps, coderefactor

--- src/extension.ts
+++ src/extension.ts
@@ -10,20 +10,17 @@ import { prettifyL2File } from "./prettifyL2File";
 import { getL2VersionAndUpdatePrompt } from "./checkL2Version";
 
 import {
-  exitLsp as exitLangServer,
-  initlizeServer as initlizeLangServer,
-  shutDownLsp as shutDownLangServer,
+  exitLangServer,
+  initializeLangServer,
+  shutDownLangServer,
 } from "./lsp/methods/lspLifecycles";
 import {
-  getDotENVS,
   lama2RegisterCompletionItemProvider,
-  replaceTextAfterEnvSelected,
-  suggestENVSFromDotEnv,
   triggerSuggestionInTemplateLiteral,
 } from "./lsp/methods/suggestEnvironmentVars";
 import { logToChannel } from "./lsp/response/generalResponse";
 
-export const MIN_VERSION_TO_CHECK = "1.5.8";
+export const MIN_VERSION_TO_CHECK = "1.5.9";
 let requestId = 1;
 
 const langServer = child_process.spawn("l2", ["--lsp"]);
@@ -31,7 +28,8 @@ const langServer = child_process.spawn("l2", ["--lsp"]);
 export function activate(context: vscode.ExtensionContext) {
   console.log('>>> Congratulations, your extension "Lama2" is now active!');
 
-  initlizeLangServer(langServer, requestId);
+  initializeLangServer(langServer, requestId);
+  getL2VersionAndUpdatePrompt(MIN_VERSION_TO_CHECK);
 
   // Level1 command pallette
   let executeCurrentFileDisposable = execCurL2File(context);
@@ -57,41 +55,13 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(generateCodeSnippetDisposable);
   console.log(">>> generateCodeSnippetDisposable is now active!");
 
-  // Automatically check L2 version on extension activation
-  let suggestEnvVariables: any;
-  let l2Version = getL2VersionAndUpdatePrompt(MIN_VERSION_TO_CHECK);
-  if (
-    l2Version === null ||
-    l2Version === undefined ||
-    semver.lt(l2Version, MIN_VERSION_TO_CHECK)
-  ) {
-    /**
-     * The L2 version, if less than v1.5.0, does not support the `l2 -e <filepath>` feature.
-     * Therefore, we use the extension to fetch variables from the l2.env file for suggestions.
-     */
-    getDotENVS();
-    suggestEnvVariables = suggestENVSFromDotEnv();
-  } else {
-    /**
-     * L2 versions greater than or equal to v1.5.0 support the `l2 -e <filepath>` feature.
-     * Therefore, we fetch variables from both the l2.env and l2config.env files for suggestions using this command.
-     */
-    suggestEnvVariables = lama2RegisterCompletionItemProvider(
-      langServer,
-      requestId
-    );
-  }
-
-  context.subscriptions.push(
-    suggestEnvVariables,
-    /**
-     * The following part is unnecessary for L2 versions > 1.5.0
-     * This method is triggered when a user selects a suggested environment variable.
-     */
-    vscode.commands.registerCommand("envoptions", (selectedEnv: string) => {
-      replaceTextAfterEnvSelected(selectedEnv);
-    })
+  // Therefore, we fetch variables from both the l2.env and l2config.env files for suggestions using this method.
+  let suggestEnvVariables = lama2RegisterCompletionItemProvider(
+    langServer,
+    requestId
   );
+
+  context.subscriptions.push(suggestEnvVariables);
 
   /**
    * Listens for text changes in the active document and triggers suggestion
--- src/lsp/request/generalRequest.ts
+++ src/lsp/request/generalRequest.ts
@@ -24,52 +24,67 @@ export interface IJSONRPCRequest {
   params: any;
 }
 
-// Function to send requests to the Language Server Protocol and read responses
-export function sendRequestToLSPReadResponse(
-  requestDetails: ILSPRequestDetails
-): Promise<IJSONRPCResponse> {
-  return new Promise((resolve, reject) => {
-    let { process, id, method, params } = requestDetails;
+// Check if the process is initialized
+function isProcessInitialized(process: any): boolean {
+  return process.stdin && process.stdout;
+}
 
-    if (!process.stdin || !process.stdout) {
+// Create a JSON RPC request
+function createJSONRPCRequest(requestDetails: ILSPRequestDetails): IJSONRPCRequest {
+  const { id, method, params = {} } = requestDetails;
+  return {
+    jsonrpc: "2.0",
+    id,
+    method,
+    params,
+  };
+}
+
+// Send a request to the process
+function sendRequestToProcess(process: any, request: IJSONRPCRequest) {
+  const requestString = JSON.stringify(request);
+  process.stdin.write(requestString + "\n");
+}
+
+// Handle the response from the process
+function handleProcessResponse(process: any, resolve: any, reject: any) {
+  const chunks: any[] = [];
+  process.stdout.on("data", (data: any) => {
+    chunks.push(data);
+    const responseData = Buffer.concat(chunks).toString();
+    try {
+      const responseObject = JSON.parse(responseData);
+      if (responseObject.jsonrpc && responseObject.id) {
+        resolve(responseObject);
+      }
+    } catch (error: any) {
+      // If JSON parsing fails, wait for a complete response
+    }
+  });
+
+  process.stdout.on("end", () => {
+    logToChannel({
+      msg: "Received incomplete response from server",
+      dataString: Buffer.concat(chunks).toString(),
+    });
+    reject(new Error("Received incomplete response from server"));
+  });
+}
+
+// Main function to send requests to the Language Server Protocol and read responses
+export function askLSP(requestDetails: ILSPRequestDetails): Promise<IJSONRPCResponse> {
+  return new Promise((resolve, reject) => {
+    const { process } = requestDetails;
+
+    if (!isProcessInitialized(process)) {
       logToChannel({ msg: "Server not initialized to make a request" });
       reject(new Error("Server not initialized to make a request"));
       return;
     }
 
-    params = params ? params : {};
-
-    const request: IJSONRPCRequest = {
-      jsonrpc: "2.0",
-      id,
-      method,
-      params,
-    };
-
+    const request = createJSONRPCRequest(requestDetails);
     logToChannel({ msg: "Sending request: ", dataObject: request });
-    const requestString = JSON.stringify(request);
-    process.stdin.write(requestString + "\n");
-
-    const chunks: any[] = [];
-    process.stdout.on("data", (data) => {
-      chunks.push(data);
-      const responseData = Buffer.concat(chunks).toString();
-      try {
-        const responseObject = JSON.parse(responseData);
-        if (responseObject.jsonrpc && responseObject.id) {
-          resolve(responseObject);
-        }
-      } catch (error: any) {
-        // If JSON parsing fails, wait for a complete response
-      }
-    });
-
-    process.stdout.on("end", () => {
-      logToChannel({
-        msg: "Received incomplete response from server",
-        dataString: Buffer.concat(chunks).toString(),
-      });
-      reject(new Error("Received incomplete response from server"));
-    });
+    sendRequestToProcess(process, request);
+    handleProcessResponse(process, resolve, reject);
   });
 }
