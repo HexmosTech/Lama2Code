5cb85d032103db8dd3933885a2ab25aea20c1de2
Lince mathew <linz07m@gmail.com>
2024-08-17 11:41:31 +0530

    handling error message and cleaning for first release

---
+++ webview/src/modules/Error.tsx
@@ -0,0 +1,17 @@
+import { Message } from "primereact/message";
+
+import React, { useRef } from "react";
+
+interface ErrorProps {
+  error: string;
+}
+
+const Error: React.FC<ErrorProps> = ({ error }) => {
+  return (
+    <div>
+      <Message severity="error" text={error} />
+    </div>
+  );
+};
+
+export default Error;
