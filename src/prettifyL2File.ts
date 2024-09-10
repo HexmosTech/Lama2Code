a62330528b59c6efebde77032a2e6ce4370c6598
Lince mathew <linz07m@gmail.com>
2024-09-10 20:06:38 +0530

    removed and refactoring

--- src/prettifyL2File.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from "vscode";
-import { exec } from "child_process";
-
-export function prettifyL2File() {
-  return vscode.commands.registerCommand("lama2.PrettifyCurrentFile", () => {
-    console.log("Executing prettify command");
-    exec(`l2 -b ${vscode.window.activeTextEditor?.document.fileName}`);
-  });
-}
